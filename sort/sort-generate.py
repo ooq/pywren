7dc1443808e362e898f11356cb56a372baf2adef
Qifan Pu <qifan.pu@gmail.com>
2017-02-27 12:09:42 -0800

    final sort generate

--- sort/sort-generate.py
+++ sort/sort-generate.py
@@ -1,8 +1,3 @@
-#from gevent import monkey
-
-#monkey.patch_socket()
-#monkey.patch_ssl()
-
 import pywren
 import boto3
 import md5
@@ -10,14 +5,8 @@ import md5
 if __name__ == "__main__":
     import logging
     import subprocess
-    #logging.basicConfig(level=logging.DEBUG)
-
-    # fh = logging.FileHandler('simpletest.log')
-    # fh.setLevel(logging.DEBUG)
-    # fh.setFormatter(pywren.wren.formatter)
-    # pywren.wren.logger.addHandler(fh)
-
-
+    import gc
+    import time
     def run_command(key):
         pywren.wrenlogging.default_config()
         logger = logging.getLogger(__name__)
@@ -25,35 +14,31 @@ if __name__ == "__main__":
 
         client = boto3.client('s3', 'us-west-2')
         client.download_file('qifan-public', 'gensort', '/tmp/condaruntime/gensort')
+        res = subprocess.check_output(["chmod",
+                                        "a+x",
+                                        "/tmp/condaruntime/gensort"])
 
-        for i in range(0,1):
+        for i in range(0,20):
             number_of_records = 1000 * 1000
             begin = key * number_of_records
-
-            res0 = subprocess.check_output(["rm", "-rf", "/tmp/condaruntime/input"])
-            res1 = subprocess.check_output(["mkdir", "-p", "/tmp/condaruntime/input"])
-            res2 = subprocess.check_output(["chmod", "a+x", "/tmp/condaruntime/gensort"])
-            res4 = subprocess.check_output(["df", "-h"])
-
-            logger.info(res4)
-            print res4
-            print 'generating'
-            # res3 = subprocess.check_output(["/tmp/condaruntime/gensort",
-            #                                 "-b"+str(begin),
-            #                                 str(number_of_records),
-            #                                 "/tmp/condaruntime/input/part-" + str(key)])
-            res4 = subprocess.check_output(["ls", "-lh", "/tmp/condaruntime"])
-            logger.info(res4)
-            print res4
-            keyname = "/input/part-" + str(key)
+            data = subprocess.check_output(["/tmp/condaruntime/gensort",
+                                            "-b"+str(begin),
+                                            str(number_of_records),
+                                            "/dev/stdout"])
+            keyname = "input/part-" + str(key)
             m = md5.new()
             m.update(keyname)
-            randomized_keyname = m.hexdigest()[:8] + keyname
-            #client.upload_file("/tmp/condaruntime/input/part-"+str(key), "sort-data-random-1t", randomized_keyname)
-            key = key+1
+            randomized_keyname = "input/" + m.hexdigest()[:8] + "-part-" + str(key)
+            client.put_object(Body = data, Bucket = "sort-data-random-1t", Key = randomized_keyname)
+            logger.info(str(key) + "th object uploaded.")
+            gc_start = time.time()
+            gc.collect()
+            gc_end = time.time()
+            logger.info("GC takes " + str(gc_end - gc_start) + " seconds.")
+            key = key + 1
 
     wrenexec = pywren.default_executor()
-    fut = wrenexec.map(run_command, range(0,10,10))
+    fut = wrenexec.map(run_command, range(0,10000,20))
 
     pywren.wait(fut)
     res = [f.result() for f in fut]
